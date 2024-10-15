c5e521e047593eaec86c21ab85a1c7f7003eb131
ali sharifi <alisharifyofficial@gmail.com>
2024-10-15 20:51:40 +0330

    :art: implement windows dns methods

--- devs_free/dns_manager/platforms/windows/base.py
+++ devs_free/dns_manager/platforms/windows/base.py
@@ -1,18 +1,32 @@
+"""
+ * devs free ( developers free ) OSS
+ * author: github.com/alisharify7
+ * email: alisharifyofficial@gmail.com
+ * license: see LICENSE for more details.
+ * Copyright (c) 2023 - ali sharifi
+ * https://github.com/alisharify7/devs_free
+"""
+
+
 import re
-import subprocess
 import os
 import json
 import pathlib
+import subprocess
+
+import questionary
 from devs_free.dns_manager.base_platforms import BasePlatformDNS
 
 
-import questionary
 
 
 class Windows(BasePlatformDNS):
+    """Window DNS manager base class"""
     @staticmethod
     def get_all_ethernet_interfaces():
-        # Using `ip link show` to get interface details
+        """this method returns all available ethernet interfaces
+        Using `ip link show`
+        """
         patterns = r"^\d+: (.+):"
         output = subprocess.check_output(["ip", "link", "show"]).decode()
         result = re.findall(patterns, output, re.MULTILINE)
@@ -21,17 +35,18 @@ class Windows(BasePlatformDNS):
         ethernet_interfaces = [iface for iface in result if "eth" in iface or "en" in iface]
         return ethernet_interfaces
 
-    def get_selected_ethernet_interfaces(self):
+    def get_main_ethernet_interfaces(self):
+        """this method returns user selected (main) ethernet interfaces"""
         if not self.does_config_exist():
             self.set_up_init_config()
 
-        with open(self.get_config_dir() / BasePlatformDNS.config_file, "r") as f:
+        with open(self.get_config_dir() / BasePlatformDNS.dns_config_file, "r") as f:
             data = json.load(fp=f)
-        return data['dns']['default-ethernet-interface']
+        return data['main-interface']
 
     @staticmethod
     def get_current_username():
-        """get current username"""
+        """get os current username"""
         command = 'whoami'
         output = subprocess.check_output([command]).decode('utf-8')
         return str(output.rsplit("\\")[-1]).strip()
@@ -45,22 +60,25 @@ class Windows(BasePlatformDNS):
         return pathlib.Path(f"C:\\Users\\{Windows.get_current_username()}")
 
     def create_config_file(self):
-        """create config file """
-        with open(file=str(self.get_config_dir() / BasePlatformDNS.config_file), mode="w") as f:
-            json.dump(BasePlatformDNS.base_config, fp=f)
+        """create dns config file from base default config """
+        with open(file=str(self.get_config_dir() / BasePlatformDNS.dns_config_file), mode="w") as f:
+            json.dump(BasePlatformDNS.dns_default_config, fp=f)
 
     def get_config_file(self):
+        """get dns config file content"""
         if not self.does_config_exist():
             raise RuntimeError("config file does not exist")
 
-        with open(file=str(self.get_config_dir() / BasePlatformDNS.config_file), mode="r") as f:
+        with open(file=str(self.get_config_dir() / BasePlatformDNS.dns_config_file), mode="r") as f:
             return json.load(f)
 
     def update_config_file(self, config_object: dict):
-        with open(self.get_config_dir() / BasePlatformDNS.config_file, "w") as f:
+        """update config file with new config """
+        with open(self.get_config_dir() / BasePlatformDNS.dns_config_file, "w") as f:
             json.dump(config_object, fp=f)
 
     def set_up_init_config(self):
+        """this method initializes DNS config file"""
         interfaces = Windows.get_all_ethernet_interfaces()
         selected_interface = questionary.select(
             choices=interfaces,
@@ -73,5 +91,5 @@ class Windows(BasePlatformDNS):
             self.create_config_file()
             config_object = self.get_config_file()
 
-        config_object['default-ethernet-interface'] = selected_interface
+        config_object['main-interface'] = selected_interface
         self.update_config_file(config_object=config_object)
