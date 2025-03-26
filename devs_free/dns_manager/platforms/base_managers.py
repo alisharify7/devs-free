ca3ca22e1779e2891d264f848df800b6a5b90200
ali sharifi <alisharifyofficial@gmail.com>
2025-03-26 21:38:09 +0330

    move base dns platform to platform dir

--- devs_free/dns_manager/platforms/base_managers.py
+++ devs_free/dns_manager/platforms/base_managers.py
@@ -14,33 +14,42 @@ from devs_free.base_platforms import BasePlatform
 class BasePlatformDNS(BasePlatform, ABC):
     """
     Base DNS manager class.
-    all dns manager classes should inherit from this class,
-    and implement the all methods that this class contain.
+    all dns manager classes should inherit from this class no matter what Dev type they are,
+    and they should implement the all methods that this class contain.
     """
 
     def get_all_ethernet_interfaces(self):
+        """get all available Ethernet interfaces on the current platform."""
         raise NotImplemented()
 
     def get_main_ethernet_interfaces(self):
+        """get current selected Ethernet interfaces on the current platform."""
         raise NotImplemented()
 
     def get_current_username(self):
+        """get current user of the OS."""
         raise NotImplemented()
 
     def does_config_exist(self):
+        """check config file exists."""
         raise NotImplemented()
 
     def get_config_dir(self):
+        """get config dir."""
         raise NotImplemented()
 
     def create_config_file(self):
+        """create config file in config dir."""
         raise NotImplemented()
 
     def get_config_file(self):
+        """get config content from config file."""
         raise NotImplemented()
 
     def update_config_file(self, config_object: dict):
+        """update config file in config file."""
         raise NotImplemented()
 
     def set_up_init_config(self):
+        """setup init config for startup. runs only once in the beginning the app"""
         raise NotImplemented()
